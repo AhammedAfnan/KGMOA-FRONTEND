3e186c515316942f689603feed55ca91cd550500
afnan <afnanballoor44@gmail.com>
2024-12-26 23:41:12 +0530

    updated

--- src/components/user/About.jsx
+++ src/components/user/About.jsx
@@ -2,39 +2,34 @@ import React from "react";
 
 export default function About() {
   const aboutImages = [
-    "/images/img1.jpg",
-    "/images/img2.jpg",
-    "/images/img3.jpg",
-    "/images/img4.jpg",
-    "/images/img5.jpg",
-    "/images/img6.jpg",
-    "/images/img7.jpg",
-    "/images/img8.jpg",
-    "/images/img9.jpg",
-    "/images/img10.jpg",
-    "/images/img11.jpg",
-    "/images/img12.jpg",
-    "/images/img14.jpg",
-    "/images/img15.jpg",
-    "/images/img16.jpg",
-    "/images/img17.jpg",
-    "/images/img18.jpg",
+    { src: "/images/img1.jpg", caption: "Secretary KGMOA Vijayapura" },
+    { src: "/images/img3.jpg", caption: "Souvenir committee heads" },
+    { src: "/images/img2.jpg", caption: "Scientific committee heads" },
+    { src: "/images/img4.jpg", caption: "Banquet committee heads" },
+    { src: "/images/img5.jpg", caption: "Transport committee heads" },
+    { src: "/images/img6.jpg", caption: "Registration committee heads" },
+    { src: "/images/img8.jpg", caption: "Chief Patrons of KGMOA" },
+    { src: "/images/img9.jpg", caption: "Co ordination committee" },
+    { src: "/images/img10.jpg", caption: "Executive committee KGMOA" },
   ];
 
   return (
     <section className="mt-10 mb-5">
       <h2 className="text-2xl font-extrabold text-center mb-6">About Us</h2>
       <div className="grid grid-cols-3 gap-4 px-4">
-        {aboutImages.map((src, index) => (
+        {aboutImages.map((item, index) => (
           <div
             key={index}
             className="bg-gray-100 rounded-md shadow-md overflow-hidden"
           >
             <img
-              src={src}
-              alt={`Image ${index + 1}`}
+              src={item.src}
+              alt={item.caption}
               className="w-full h-80 object-cover"
             />
+            <div className="p-2 bg-white text-center">
+              <p className="font-bold text-gray-700">{item.caption}</p>
+            </div>
           </div>
         ))}
       </div>
